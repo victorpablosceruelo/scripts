7b4d2a1fdf14145ab669ced16ddd46d2ef7dee33
VICTOR PABLOS CERUELO <victor.pablos@externos.correo.gob.es>
2020-10-07 09:36:33 +0200

    typos

--- ot/low_level_tasks_scripts/installThisRepoInAllHosts.sh
+++ ot/low_level_tasks_scripts/installThisRepoInAllHosts.sh
@@ -8,6 +8,7 @@ allHosts=$(${DIRNAME}/getAllHosts.sh)
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+USERNAME_AND_PWD="${1}"
 
 for host in ${allHosts}; do
     echo -e "${RED}${host}${NC}: "
@@ -16,13 +17,13 @@ for host in ${allHosts}; do
     echo " "
     ssh root@${host} "git config --global http.sslVerify false"
     echo " "
-    ssh root@${host} "[ -d /opt/scripts_oficina_tecnica_de_proyectos/.git ] || rm -fRv /opt/scripts_oficina_tecnica_de_proyectos/"
+    ssh root@${host} "[ -d /opt/scripts/.git ] || rm -fRv /opt/scripts/"
     echo " "
-    echo "Updating repository in /opt/scripts_oficina_tecnica_de_proyectos : "
+    echo "Updating repository in /opt/scripts : "
     echo " "
-    ssh root@${host} "[ -d /opt/scripts_oficina_tecnica_de_proyectos/.git ] || git clone http://Jenkins:1s3v68ftfYjHqVE5_-iM@gitlab-ic.scae.redsara.es/OFICINA_TECNICA/scripts_oficina_tecnica_de_proyectos.git /opt/scripts_oficina_tecnica_de_proyectos/"
+    ssh root@${host} "[ -d /opt/scripts/.git ] || git clone http://${USERNAME_AND_PWD}@gitlab-ic.scae.redsara.es/OFICINA_TECNICA/scripts.git /opt/scripts/"
     echo " "
-    ssh root@${host} "cd /opt/scripts_oficina_tecnica_de_proyectos/ ; git checkout master ; git pull"
+    ssh root@${host} "cd /opt/scripts/ ; git checkout master ; git pull"
     echo " " 
 done
 
