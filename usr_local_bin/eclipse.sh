a87f433b2fe8699acca9f109770701ecd1e46866
Victor Pablos Ceruelo <victorpablosceruelo@gmail.com>
2020-11-30 17:51:25 +0100

    Eclipse script.

--- usr_local_bin/eclipse.sh
+++ usr_local_bin/eclipse.sh
@@ -4,17 +4,19 @@ echo "Running eclipse ... "
 
 killall -9 eclipse
 
-nc -zv 127.0.0.1 3128
-if [ 0 -eq $? ]; then
-    PROXY_IP=127.0.0.1
-    PROXY_PORT=3128
-else
-    PROXY_IP=10.1.0.222
-    PROXY_PORT=8080
-fi
+# nc -zv 127.0.0.1 3128
+# if [ 0 -eq $? ]; then
+#    PROXY_IP=127.0.0.1
+#    PROXY_PORT=3128
+#else
+#    PROXY_IP=10.1.0.222
+#    PROXY_PORT=8080
+#fi
 
-echo "Proxy chosen: ${PROXY_IP} ${PROXY_PORT} "
-echo " "
+#echo "Proxy chosen: ${PROXY_IP} ${PROXY_PORT} "
+#echo " "
+
+ECLIPSE_PATH=$(find . -name eclipse -type f | head -n 1)
 
 ECLIPSE_OPTS="-Dhttp.proxyHost=${PROXY_IP} -Dhttp.proxyPort=${PROXY_PORT} -Dhttps.proxyHost=${PROXY_IP} -Dhttps.proxyPort=${PROXY_PORT} -DsocksProxyHost= -DsocksProxyPort=  -Djava.net.preferIPv4Stack=true -Djava.net.useSystemProxies=true -Dorg.eclipse.ecf.provider.filetransfer.excludeContributors=org.eclipse.ecf.provider.filetransfer.httpclient4 "
 # -Dhttp.proxyUser=<user>
@@ -22,5 +24,5 @@ ECLIPSE_OPTS="-Dhttp.proxyHost=${PROXY_IP} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp
 # -Dhttp.nonProxyHosts=localhost|127.0.0.1
 
 # socks_proxy=127.0.0.1:3128 
-http_proxy=${PROXY_IP}:${PROXY_PORT} https_proxy=${PROXY_IP}:${PROXY_PORT} ~/eclipse/eclipse -clean -refresh ${ECLIPSE_OPTS} &> /tmp/eclipse.log 
+http_proxy=${PROXY_IP}:${PROXY_PORT} https_proxy=${PROXY_IP}:${PROXY_PORT} ${ECLIPSE_PATH} -clean -refresh ${ECLIPSE_OPTS} &> /tmp/eclipse.log 
 
