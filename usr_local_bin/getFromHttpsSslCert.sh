096b8326c6b37956ea7aed6c317cabd6926c920f
Victor Pablos Ceruelo <victorpablosceruelo@gmail.com>
2020-12-11 22:26:56 +0100

    Correct way to generate cert files to import.

--- usr_local_bin/getFromHttpsSslCert.sh
+++ usr_local_bin/getFromHttpsSslCert.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 ME=$(basename ${0})
-CERTS_FILE="certificates.cer"
 
 if [ -z "${1}" ] || [ "" == "${1}" ]; then
 	echo "usage: ${ME} SITE "
 	exit 0
 fi
 
-rm -fv ${ME}.log
-rm -fv ${CERTS_FILE}
+FILE_NAME=$(echo ${1} | sed 's/\./_/g')
+CERTS_FILE="${FILE_NAME}_cert.cer"
+LOGS_FILE="${FILE_NAME}_cert.log"
+
+rm -fv ${LOGS_FILE} ${CERTS_FILE}
 
 # SITE="${1}"
 SITE_WEB="${1}"
 
 # -servername ${SITE}
 
-echo | openssl s_client -showcerts -connect ${SITE_WEB}:443 2>/dev/null >> ${ME}.log 
-cat ${ME}.log | awk '/-----BEGIN CERTIFICATE-----/, /-----END CERTIFICATE-----/' >> ${CERTS_FILE}
+echo | openssl s_client -showcerts -connect ${SITE_WEB}:443 2>/dev/null >> ${LOGS_FILE}
+cat ${LOGS_FILE} | awk '/-----BEGIN CERTIFICATE-----/, /-----END CERTIFICATE-----/' >> ${CERTS_FILE}
 
 echo "Result: ${CERTS_FILE} "
-echo "Logs:   ${ME}.log "
+echo "Logs:   ${LOGS_FILE} "
