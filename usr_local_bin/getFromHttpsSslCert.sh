d1a8e5463b0b72b4f403d0d80e7d10d3f8bbfe6d
Victor Pablos Ceruelo <victorpablosceruelo@gmail.com>
2020-12-11 21:44:46 +0100

    Correct way to obtain chained certs.

--- usr_local_bin/getFromHttpsSslCert.sh
+++ usr_local_bin/getFromHttpsSslCert.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 ME=$(basename ${0})
+CERTS_FILE="certificates.cer"
 
-if [ -z "${1}" ] || [ -z "${2}" ] || [ "" == "${1}" ] || [ "" == "${2}" ]; then
-	echo "usage: ${ME} SITE SITE_WEB"
+if [ -z "${1}" ] || [ "" == "${1}" ]; then
+	echo "usage: ${ME} SITE "
 	exit 0
 fi
 
-SITE="${1}"
-SITE_WEB="${2}"
+rm -fv ${ME}.log
+rm -fv ${CERTS_FILE}
 
-echo | openssl s_client -showcerts -servername ${SITE} -connect ${SITE_WEB}:443 2>/dev/null | awk '/-----BEGIN CERTIFICATE-----/, /-----END CERTIFICATE-----/' >> company-internal-certificate.cer
+# SITE="${1}"
+SITE_WEB="${1}"
 
+# -servername ${SITE}
+
+echo | openssl s_client -showcerts -connect ${SITE_WEB}:443 2>/dev/null >> ${ME}.log 
+cat ${ME}.log | awk '/-----BEGIN CERTIFICATE-----/, /-----END CERTIFICATE-----/' >> ${CERTS_FILE}
+
+echo "Result: ${CERTS_FILE} "
+echo "Logs:   ${ME}.log "
